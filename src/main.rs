9cbc0c2de09bd69826bece04746b4194f3f2587f
Justin Moon <mail@justinmoon.com>
2020-05-15 02:22:03 -0500

    Generate address and write to screen

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,46 @@ use embedded_graphics::{
     fonts::{Font6x8, Text},
     pixelcolor::BinaryColor,
     prelude::*,
-    primitives::{Circle, Line},
-    style::{PrimitiveStyle, TextStyle},
+    style::TextStyle,
 };
+
 use embedded_graphics_simulator::{
     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
 };
 
+use bitcoin::network::constants::Network;
+use bitcoin::secp256k1::rand::thread_rng;
+use bitcoin::secp256k1::Secp256k1;
+use bitcoin::util::address::Address;
+use bitcoin::util::key;
+
+fn get_address() -> Address {
+    // Generate random key pair
+    let s = Secp256k1::new();
+    let public_key = key::PublicKey {
+        compressed: true,
+        key: s.generate_keypair(&mut thread_rng()).1,
+    };
+
+    // Return pay-to-pubkey-hash address
+    let address = Address::p2pkh(&public_key, Network::Bitcoin);
+    return address;
+}
+
 fn main() -> Result<(), core::convert::Infallible> {
-    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(129, 129));
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(300, 300));
 
-    let line_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-
-    Circle::new(Point::new(64, 64), 64)
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Line::new(Point::new(64, 64), Point::new(0, 64))
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Line::new(Point::new(64, 64), Point::new(80, 80))
-        .into_styled(line_style)
-        .draw(&mut display)?;
-
-    Text::new("Hello World!", Point::new(5, 50))
+    // Generate address and draw on screen
+    let address = get_address();
+    Text::new(&address.to_string(), Point::new(5, 5))
         .into_styled(TextStyle::new(Font6x8, BinaryColor::On))
         .draw(&mut display)?;
 
+    // Create window
     let output_settings = OutputSettingsBuilder::new()
         .theme(BinaryColorTheme::OledBlue)
         .build();
-    Window::new("Hello World", &output_settings).show_static(&display);
+    Window::new(&address.to_string(), &output_settings).show_static(&display);
 
     Ok(())
 }
